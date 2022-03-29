8046b9374cfc25efe90626472c8b5cbc197f816f
Laura Duque <duquelaura295@gmail.com>
2022-03-29 17:12:01 -0500

    create a endpoint for home route

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const express = require("express");
 
 const app = express();
 
-app.get("/movies", (req, res) => res.send(moviesMocks));
+app.get("/", (req, res) => {
+  res.status(200).send("home");
+});
+app.get("/movies", (req, res) => {
+  res.status(200).send(moviesMocks);
+});
 
 app.listen(3000, () => console.log(`server running on http://localhost:3000`));
